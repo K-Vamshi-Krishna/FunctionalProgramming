309b5b63790e89dccc8e975dfa1fde15e748f9db
Vamshi <k.vamshi.krish@gmail.com>
2015-04-04 21:44:41 +0530

    different main

--- LearnYouHaskell/helloworld.hs
+++ LearnYouHaskell/helloworld.hs
@@ -1,3 +1,5 @@
+import Data.Char
+import Control.Monad
 -- main = putStrLn "hello,world!"
 
 --main :: IO ()
@@ -6,14 +8,44 @@
 --		name <- getLine
 --		putStrLn ("Hey " ++ name ++ " ,you rock!")
 
-main :: IO ()
-main = do
-		putStrLn "What's your first name?"
-		firstName <- getLine
-		putStrLn "What's your last name ?"
-		lastName <- getLine
-		let
-			bigFirstName = map toUpper firstName
-			bigLastName = map toUpper lastName
-		putStrLn $ "Hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
+--main :: IO ()
+--main = do
+--		putStrLn "What's your first name?"
+--		firstName <- getLine
+--		putStrLn "What's your last name ?"
+--		lastName <- getLine
+--		let
+--			bigFirstName = map toUpper firstName
+--			bigLastName = map toUpper lastName
+--		putStrLn $ "Hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
 
+
+--main :: IO ()
+--main = do 
+--	line <- getLine
+--	if null line
+--		then return ()
+--	else do
+--		putStrLn $ reverseWords line
+--		main
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
+
+
+-- take 3 intputs
+--main = do 
+--	rs <- sequence [getLine,getLine,getLine]
+--	print rs
+
+
+main = forever $ do 
+	putStr' "Give me some input\n"
+	l <- getLine
+	putStrLn $ map toUpper l
+
+putStr' :: String -> IO()
+putStr'[] = return ()
+putStr' (x:xs) = do
+			putChar x
+			putStr' xs
